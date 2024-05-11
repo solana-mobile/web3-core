48413338c326a6ff87f8577dcafb3d51599b1d4e
Marco Martinez <funkatronicsmail@gmail.com>
2024-05-11 13:00:20 -0600

    Update Signer.kt

--- core/src/commonMain/kotlin/com/solana/signer/Signer.kt
+++ core/src/commonMain/kotlin/com/solana/signer/Signer.kt
@@ -1,7 +1,9 @@
 package com.solana.signer
 
+import com.solana.publickey.PublicKey
+
 interface Signer {
-    val publicKey: ByteArray
+    val publicKey: PublicKey
     val ownerLength: Number
     val signatureLength: Number
     suspend fun signPayload(payload: ByteArray): ByteArray
