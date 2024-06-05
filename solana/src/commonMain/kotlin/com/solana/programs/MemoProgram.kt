54923c4dacc87655ef26ec983021aad20a29be85
funkatronics <funkatronicsmail@gmail.com>
2024-06-05 16:16:14 -0600

    add Memo program + cleanup

---
+++ solana/src/commonMain/kotlin/com/solana/programs/MemoProgram.kt
@@ -0,0 +1,18 @@
+package com.solana.programs
+
+import com.solana.publickey.SolanaPublicKey
+import com.solana.transaction.AccountMeta
+import com.solana.transaction.TransactionInstruction
+import kotlin.jvm.JvmStatic
+
+object MemoProgram {
+    @JvmStatic
+    val PROGRAM_ID = SolanaPublicKey.from("Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo")
+
+    @JvmStatic
+    fun publishMemo(account: SolanaPublicKey, memo: String): TransactionInstruction =
+        TransactionInstruction(PROGRAM_ID,
+            listOf(AccountMeta(account, true, false)),
+            memo.encodeToByteArray()
+        )
+}
--- solana/src/commonMain/kotlin/com/solana/programs/SystemProgram.kt
+++ solana/src/commonMain/kotlin/com/solana/programs/SystemProgram.kt
@@ -11,25 +11,25 @@ object SystemProgram {
     @JvmStatic
     val PROGRAM_ID = SolanaPublicKey.from("11111111111111111111111111111111")
 
-    const val PROGRAM_INDEX_CREATE_ACCOUNT = 0
-    const val PROGRAM_INDEX_TRANSFER = 2
+    private const val PROGRAM_INDEX_CREATE_ACCOUNT = 0
+    private const val PROGRAM_INDEX_TRANSFER = 2
 
     @JvmStatic
     fun transfer(
         fromPublicKey: SolanaPublicKey,
         toPublickKey: SolanaPublicKey,
         lamports: Long
-    ): TransactionInstruction {
-        val keys = ArrayList<AccountMeta>()
-        keys.add(AccountMeta(fromPublicKey, true, true))
-        keys.add(AccountMeta(toPublickKey, false, true))
-
-        // 4 byte instruction index + 8 bytes lamports
-        val data = BorshEncoder()
-        data.encodeInt(PROGRAM_INDEX_TRANSFER)
-        data.encodeLong(lamports)
-        return TransactionInstruction(PROGRAM_ID, keys, data.borshEncodedBytes)
-    }
+    ): TransactionInstruction =
+        TransactionInstruction(PROGRAM_ID,
+            listOf(
+                AccountMeta(fromPublicKey, true, true),
+                AccountMeta(toPublickKey, false, true)
+            ),
+            BorshEncoder().apply {
+                encodeInt(PROGRAM_INDEX_TRANSFER)
+                encodeLong(lamports)
+            }.borshEncodedBytes
+        )
 
     fun createAccount(
         fromPublicKey: SolanaPublicKey,
@@ -37,15 +37,17 @@ object SystemProgram {
         lamports: Long,
         space: Long,
         programId: SolanaPublicKey
-    ): TransactionInstruction {
-        val keys = ArrayList<AccountMeta>()
-        keys.add(AccountMeta(fromPublicKey, true, true))
-        keys.add(AccountMeta(newAccountPublickey, true, true))
-        val data = BorshEncoder()
-        data.encodeInt(PROGRAM_INDEX_CREATE_ACCOUNT)
-        data.encodeLong(lamports)
-        data.encodeLong(space)
-        data.encodeSerializableValue(ByteArraySerializer(), programId.bytes)
-        return TransactionInstruction(PROGRAM_ID, keys, data.borshEncodedBytes)
-    }
+    ): TransactionInstruction =
+        TransactionInstruction(PROGRAM_ID,
+            listOf(
+                AccountMeta(fromPublicKey, true, true),
+                AccountMeta(newAccountPublickey, true, true)
+            ),
+            BorshEncoder().apply {
+                encodeInt(PROGRAM_INDEX_CREATE_ACCOUNT)
+                encodeLong(lamports)
+                encodeLong(space)
+                encodeSerializableValue(ByteArraySerializer(), programId.bytes)
+            }.borshEncodedBytes
+        )
 }
