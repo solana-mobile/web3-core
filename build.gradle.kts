9b848ec187b07adc9bafdd46d7f3cf3293fdb69d
funkatronics <funkatronicsmail@gmail.com>
2024-06-05 11:16:55 -0600

    remove commented code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-//buildscript {
-//    dependencies {
-//        classpath("com.android.tools.build:gradle:8.1.0")
-//    }
-//}
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.publish) apply false
