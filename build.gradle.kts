1fce181e3de1e6f60c0d85406976867bc249fbae
funkatronics <funkatronicsmail@gmail.com>
2024-06-05 09:54:56 -0600

    pull everything to new branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
+//buildscript {
+//    dependencies {
+//        classpath("com.android.tools.build:gradle:8.1.0")
+//    }
+//}
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.publish) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
 
 //tasks.register("clean", Delete::class) {
---
+++ mwa-signer/build.gradle.kts
@@ -0,0 +1,48 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.solana.signer.mwa"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    api(project(mapOf("path" to ":solana")))
+    implementation(libs.mwa.clientlib.ktx)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.multimult)
+
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.androidx.activity.ktx)
+    testImplementation(libs.mockito.inline)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.mwa.walletlib)
+    testImplementation(libs.pkc)
+
+    testImplementation("org.robolectric:robolectric:4.12.2")
+}
