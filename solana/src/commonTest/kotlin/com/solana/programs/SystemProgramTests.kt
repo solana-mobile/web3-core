a97c0394484d0a1d7a513f1a259a99462600eabf
Marco Martinez <funkatronicsmail@gmail.com>
2024-06-07 13:47:38 -0600

    Add Solana System Programs (#22)
    * add System Program
    
    * add Memo program + cleanup
    
    * add missing JvmStatic annotation
    
    * fix memo program
    
    * add tests
    
    * add validator to ci
    
    * add transaction confirmation
    
    * start the validator on CI
    
    * edit name
    
    * run validator in bg
    
    * stop validator after test
    
    * check
    
    * remove stop step

---
+++ solana/src/commonTest/kotlin/com/solana/programs/SystemProgramTests.kt
@@ -0,0 +1,100 @@
+package com.solana.programs
+
+import com.solana.config.TestConfig
+import com.solana.publickey.SolanaPublicKey
+import com.solana.transaction.Message
+import com.solana.transaction.Transaction
+import com.solana.util.RpcClient
+import diglol.crypto.Ed25519
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class SystemProgramTests {
+
+    @Test
+    fun `createAccount successfully creates account`() = runTest {
+        // given
+        val payerKeyPair = Ed25519.generateKeyPair()
+        val newAccountKeyPair = Ed25519.generateKeyPair()
+        val payerPubkey = SolanaPublicKey(payerKeyPair.publicKey)
+        val newAccountPubkey = SolanaPublicKey(newAccountKeyPair.publicKey)
+        val rpc = RpcClient(TestConfig.RPC_URL)
+
+        // when
+        val airdropResponse = rpc.requestAirdrop(payerPubkey, 0.1f)
+
+        val rentExemptBalanceResponse = rpc.getMinBalanceForRentExemption(0)
+
+        val blockhashResponse = rpc.getLatestBlockhash()
+
+        val transaction = Message.Builder()
+            .setRecentBlockhash(blockhashResponse.result!!.blockhash)
+            .addInstruction(SystemProgram.createAccount(payerPubkey,
+                newAccountPubkey,
+                rentExemptBalanceResponse.result!!,
+                0L,
+                SystemProgram.PROGRAM_ID
+            ))
+            .build().run {
+                val payerSig = Ed25519.sign(payerKeyPair, serialize())
+                val newAccountSig = Ed25519.sign(newAccountKeyPair, serialize())
+                Transaction(listOf(payerSig, newAccountSig), this)
+            }
+
+        withContext(Dispatchers.Default.limitedParallelism(1)) {
+            rpc.sendAndConfirmTransaction(transaction)
+        }
+
+        val response = rpc.getBalance(newAccountPubkey)
+
+        // then
+        assertNull(airdropResponse.error)
+        assertNotNull(airdropResponse.result)
+        assertNull(response.error)
+        assertNotNull(response.result)
+        assertEquals(rentExemptBalanceResponse.result!!, response.result)
+    }
+
+    @Test
+    fun `transfer successfully transfers funds`() = runTest {
+        // given
+        val payerKeyPair = Ed25519.generateKeyPair()
+        val receiverKeyPair = Ed25519.generateKeyPair()
+        val payerPubkey = SolanaPublicKey(payerKeyPair.publicKey)
+        val receiverPubkey = SolanaPublicKey(receiverKeyPair.publicKey)
+        val rpc = RpcClient(TestConfig.RPC_URL)
+        val balance = 10000000L // lamports
+
+        // when
+        val airdropResponse = rpc.requestAirdrop(payerPubkey, 0.1f)
+        val blockhashResponse = rpc.getLatestBlockhash()
+
+        val transaction = Message.Builder()
+            .setRecentBlockhash(blockhashResponse.result!!.blockhash)
+            .addInstruction(SystemProgram.transfer(payerPubkey, receiverPubkey, balance))
+            .build().run {
+                val sig = Ed25519.sign(payerKeyPair, serialize())
+                Transaction(listOf(sig), this)
+            }
+
+        withContext(Dispatchers.Default.limitedParallelism(1)) {
+            rpc.sendAndConfirmTransaction(transaction)
+        }
+
+        val response = rpc.getBalance(receiverPubkey)
+
+        // then
+        assertNull(airdropResponse.error)
+        assertNotNull(airdropResponse.result)
+        assertNull(response.error)
+        assertNotNull(response.result)
+        assertEquals(balance, response.result!!)
+    }
+}
---
+++ solana/src/commonTest/kotlin/com/solana/util/RpcClient.kt
@@ -0,0 +1,197 @@
+package com.solana.util
+
+import com.funkatronics.encoders.Base58
+import com.solana.networking.HttpNetworkDriver
+import com.solana.networking.HttpRequest
+import com.solana.networking.Rpc20Driver
+import com.solana.publickey.SolanaPublicKey
+import com.solana.rpccore.JsonRpc20Request
+import com.solana.transaction.Transaction
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.util.date.*
+import io.ktor.utils.io.core.*
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.withTimeout
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.nullable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.*
+import kotlin.math.pow
+
+class RpcClient(val rpcDriver: Rpc20Driver) {
+
+    constructor(url: String, networkDriver: HttpNetworkDriver = KtorHttpDriver()): this(Rpc20Driver(url, networkDriver))
+
+    suspend fun requestAirdrop(address: SolanaPublicKey, amountSol: Float) =
+        rpcDriver.makeRequest(
+            AirdropRequest(address, (amountSol*10f.pow(9)).toLong()),
+            String.serializer()
+        )
+
+    suspend fun getBalance(address: SolanaPublicKey, commitment: String = "confirmed") =
+        rpcDriver.makeRequest(BalanceRequest(address, commitment), SolanaResponseSerializer(Long.serializer()))
+
+    suspend fun getMinBalanceForRentExemption(size: Long, commitment: String? = null) =
+        rpcDriver.makeRequest(RentExemptBalanceRequest(size, commitment), Long.serializer())
+
+    suspend fun getLatestBlockhash() =
+        rpcDriver.makeRequest(LatestBlockhashRequest(), SolanaResponseSerializer(BlockhashResponse.serializer()))
+
+    suspend fun sendTransaction(transaction: Transaction) =
+        rpcDriver.makeRequest(SendTransactionRequest(transaction), String.serializer())
+
+    suspend fun sendAndConfirmTransaction(transaction: Transaction) =
+        sendTransaction(transaction).apply {
+            result?.let { confirmTransaction(it) }
+        }
+
+    suspend fun getSignatureStatuses(signatures: List<String>) =
+        rpcDriver.makeRequest(SignatureStatusesRequest(signatures),
+            SolanaResponseSerializer(ListSerializer(SignatureStatus.serializer().nullable)))
+
+    suspend fun confirmTransaction(
+        signature: String,
+        commitment: String = "confirmed",
+        timeout: Long = 15000
+    ): Result<String> = withTimeout(timeout) {
+        suspend fun getStatus() =
+            getSignatureStatuses(listOf(signature))
+                .result?.first()
+
+        // wait for desired transaction status
+        while(getStatus()?.confirmationStatus != commitment) {
+
+            // wait a bit before retrying
+            val millis = getTimeMillis()
+            var inc = 0
+            while(getTimeMillis() - millis < 300 && isActive) { inc++ }
+
+            if (!isActive) break // breakout after timeout
+        }
+
+        Result.success(signature)
+    }
+
+    class SolanaResponseSerializer<R>(dataSerializer: KSerializer<R>)
+        : KSerializer<R?> {
+        private val serializer = WrappedValue.serializer(dataSerializer)
+        override val descriptor: SerialDescriptor = serializer.descriptor
+
+        override fun serialize(encoder: Encoder, value: R?) =
+            encoder.encodeSerializableValue(serializer, WrappedValue(value))
+
+        override fun deserialize(decoder: Decoder): R? =
+            decoder.decodeSerializableValue(serializer).value
+    }
+
+    @Serializable
+    class WrappedValue<V>(val value: V?)
+
+    class KtorHttpDriver : HttpNetworkDriver {
+        override suspend fun makeHttpRequest(request: HttpRequest): String =
+            HttpClient().use { client ->
+                client.request(request.url) {
+                    method = HttpMethod.parse(request.method)
+                    request.properties.forEach { (k, v) ->
+                        header(k, v)
+                    }
+                    setBody(request.body)
+                }.bodyAsText().apply {
+                    println(this)
+                }
+            }
+    }
+
+    class AirdropRequest(address: SolanaPublicKey, lamports: Long, requestId: String = "1")
+        : JsonRpc20Request(
+            method = "requestAirdrop",
+            params = buildJsonArray {
+                add(address.base58())
+                add(lamports)
+            },
+            id = requestId
+        )
+
+    class BalanceRequest(address: SolanaPublicKey, commitment: String = "confirmed", requestId: String = "1")
+        : JsonRpc20Request(
+            method = "getBalance",
+            params = buildJsonArray {
+                add(address.base58())
+                addJsonObject {
+                    put("commitment", commitment)
+                }
+            },
+            requestId
+        )
+
+    class LatestBlockhashRequest(commitment: String = "confirmed", requestId: String = "1")
+        : JsonRpc20Request(
+            method = "getLatestBlockhash",
+            params = buildJsonArray {
+                addJsonObject {
+                    put("commitment", commitment)
+                }
+            },
+            requestId
+        )
+
+    @Serializable
+    class BlockhashResponse(
+        val blockhash: String,
+        val lastValidBlockHeight: Long
+    )
+
+    class SendTransactionRequest(transaction: Transaction, skipPreflight: Boolean = true, requestId: String = "1")
+        : JsonRpc20Request(
+            method = "sendTransaction",
+            params = buildJsonArray {
+                add(Base58.encodeToString(transaction.serialize()))
+                addJsonObject {
+                    put("skipPreflight", skipPreflight)
+                }
+            },
+            requestId
+        )
+
+    class SignatureStatusesRequest(transactionIds: List<String>, searchTransactionHistory: Boolean = false, requestId: String = "1")
+        : JsonRpc20Request(
+            method = "getSignatureStatuses",
+            params = buildJsonArray {
+                addJsonArray { transactionIds.forEach { add(it) } }
+                addJsonObject {
+                    put("searchTransactionHistory", searchTransactionHistory)
+                }
+            },
+            requestId
+        )
+
+    @Serializable
+    data class SignatureStatus(
+        val slot: Long,
+        val confirmations: Long?,
+        var err: JsonObject?,
+        var confirmationStatus: String?
+    )
+
+    class RentExemptBalanceRequest(size: Long, commitment: String? = null, requestId: String = "1")
+        : JsonRpc20Request(
+            method = "getMinimumBalanceForRentExemption",
+            params = buildJsonArray {
+                add(size)
+                commitment?.let {
+                    addJsonObject {
+                        put("commitment", commitment)
+                    }
+                }
+            },
+            requestId
+        )
+}
