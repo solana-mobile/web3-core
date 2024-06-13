e5a920560cd0584cbabf7de6199e6b29dd9ec658
Marco Martinez <funkatronicsmail@gmail.com>
2024-06-13 17:38:51 -0600

    Add JSON serialization support for SolanaPublicKey (#30)
    * add json serialization support for SolanaPublicKey
    
    * fix transaction encoding

--- solana/src/commonMain/kotlin/com/solana/publickey/SolanaPublicKey.kt
+++ solana/src/commonMain/kotlin/com/solana/publickey/SolanaPublicKey.kt
@@ -1,12 +1,19 @@
 package com.solana.publickey
 
 import com.funkatronics.encoders.Base58
+import com.funkatronics.kborsh.BorshDecoder
+import com.funkatronics.kborsh.BorshEncoder
 import com.solana.serialization.ByteStringSerializer
+import com.solana.serialization.TransactionDecoder
+import com.solana.serialization.TransactionEncoder
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
 
 @Serializable(with=SolanaPublicKeySerializer::class)
 open class SolanaPublicKey(final override val bytes: ByteArray) : PublicKey {
@@ -35,13 +42,35 @@ open class SolanaPublicKey(final override val bytes: ByteArray) : PublicKey {
 }
 
 object SolanaPublicKeySerializer : KSerializer<SolanaPublicKey> {
-    private val delegate = ByteStringSerializer(SolanaPublicKey.PUBLIC_KEY_LENGTH)
-    override val descriptor: SerialDescriptor = delegate.descriptor
+    private val borshDelegate = ByteStringSerializer(SolanaPublicKey.PUBLIC_KEY_LENGTH)
+    private val jsonDelegate = String.serializer()
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("SolanaPublicKey")
 
     override fun deserialize(decoder: Decoder): SolanaPublicKey =
-        SolanaPublicKey(decoder.decodeSerializableValue(delegate))
+        when (decoder) {
+            is BorshDecoder, is TransactionDecoder ->
+                SolanaPublicKey(decoder.decodeSerializableValue(borshDelegate))
+            is JsonDecoder ->
+                SolanaPublicKey.from(decoder.decodeSerializableValue(jsonDelegate))
+            else ->
+                runCatching {
+                    SolanaPublicKey.from(decoder.decodeSerializableValue(jsonDelegate))
+                }.getOrElse {
+                    SolanaPublicKey(decoder.decodeSerializableValue(borshDelegate))
+                }
+        }
 
-    override fun serialize(encoder: Encoder, value: SolanaPublicKey) {
-        encoder.encodeSerializableValue(delegate, value.bytes)
-    }
+    override fun serialize(encoder: Encoder, value: SolanaPublicKey) =
+        when (encoder) {
+            is BorshEncoder, is TransactionEncoder ->
+                encoder.encodeSerializableValue(borshDelegate, value.bytes)
+            is JsonDecoder ->
+                encoder.encodeSerializableValue(jsonDelegate, value.base58())
+            else ->
+                runCatching {
+                    encoder.encodeSerializableValue(jsonDelegate, value.base58())
+                }.getOrElse {
+                    encoder.encodeSerializableValue(borshDelegate, value.bytes)
+                }
+        }
 }
