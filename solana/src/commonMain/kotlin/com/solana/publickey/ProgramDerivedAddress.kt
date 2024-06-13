7ac48862962460df5fbd2596c877cc7bf4700478
funkatronics <funkatronicsmail@gmail.com>
2024-06-13 10:40:32 -0600

    more cleanup

--- solana/src/commonMain/kotlin/com/solana/publickey/ProgramDerivedAddress.kt
+++ solana/src/commonMain/kotlin/com/solana/publickey/ProgramDerivedAddress.kt
@@ -8,22 +8,25 @@ class ProgramDerivedAddress private constructor(bytes: ByteArray, val nonce: UBy
 
     private constructor(publicKey: PublicKey, nonce: UByte) : this(publicKey.bytes, nonce)
 
-    suspend fun ProgramDerivedAddress(bytes: ByteArray, nonce: UByte) {
-        require(!bytes.isOnCurve()) { "Provided public key is not a PDA, address must be off Ed25519 curve" }
-        return ProgramDerivedAddress(bytes, nonce)
-    }
-
-    suspend fun ProgramDerivedAddress(publicKey: PublicKey, nonce: UByte) =
-        ProgramDerivedAddress(publicKey.bytes, nonce)
-
     companion object {
         @JvmStatic
         suspend fun find(seeds: List<ByteArray>, programId: PublicKey): Result<ProgramDerivedAddress> {
             for (bump in 255 downTo 0) {
+                println(bump)
                 val result = Program.createProgramAddress(seeds + byteArrayOf(bump.toByte()), programId)
                 if (result.isSuccess) return result.map { ProgramDerivedAddress(it, bump.toUByte()) }
             }
             return Result.failure(Error("Unable to find valid derived address for provided seeds"))
         }
+
+        @JvmStatic
+        suspend fun create(bytes: ByteArray, nonce: UByte): ProgramDerivedAddress {
+            require(!bytes.isOnCurve()) { "Provided public key is not a PDA, address must be off Ed25519 curve" }
+            return ProgramDerivedAddress(bytes, nonce)
+        }
+
+        @JvmStatic
+        suspend fun create(publicKey: PublicKey, nonce: UByte) =
+            ProgramDerivedAddress(publicKey.bytes, nonce)
     }
 }
